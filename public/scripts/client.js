bc86f7ccba2ac4690a54639e9a2be2b835763c65
Vgalii <veronicaggalii@gmail.com>
2021-12-15 15:28:31 -0500

    Create function to append html elements to index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,41 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const createTweetElement = function(tweetData){
+  const $tweet = $(`<article>
+  <header>
+    <i ${tweetData.user.avatars}></i>
+    <h4>${tweetData.user.name}</h4>
+    <div>${tweetData.user.handle}</div>
+  </header>
+  <p>${tweetData.content.text}</p>
+  <footer>
+    <h6>10 days ago</h6>
+    <div class="icons">
+      <div><i class="far fa-flag"></i></div>
+      <div><i class="fas fa-retweet"></i></div>
+      <div><i class="fas fa-heart"></i></div>
+    </div>
+  </footer>
+</article>`);
+return $tweet;
+}
+
+$(document).ready(function(){
+  const $tweet = createTweetElement(tweetData);
+  console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet);
+})
 
